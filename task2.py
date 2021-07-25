67642abf454f45e6b462228e9147d9fe3e1abfe9
Andrey <goldfrutty@mail.ru>
2021-07-25 13:03:29 +0300

    HomeWork lesson#4

--- task2.py
+++ task2.py
@@ -1,23 +1,9 @@
-my_list = []
-el = None
-i = 0
-
-while (el != "-"):
-    el = input("Напишите что вы хотите добавить в список: ") #eсли хотите перестать добавлять элементы, напишите "-"
-    if el == "-":
-        break
-    my_list.append(el)
-    print(my_list)
-
-if len(my_list) % 2 == 0:
-    while i < len(my_list):
-        my_list[i], my_list[i+1] = my_list[i+1], my_list[i]
-        i += 2
-else:
-    copy1 = my_list[-1] #занесение последнего элемента в переменную
-    my_list.remove(copy1) #удаление последнего элемента
-    while i < len(my_list):
-        my_list[i], my_list[i+1] = my_list[i+1], my_list[i]
-        i += 2
-    my_list.append(copy1) #востановление последнего элемента
-print(my_list)
+def information():
+    name = input("Введите имя: ")
+    surname = input("Введите фамилию: ")
+    year = int(input("Введите год рождения: "))
+    city = input("Введите город проживания: ")
+    email = input("Введите email: ")
+    telephone = input("Введите номер телефона: ")
+    print(name, surname, year, city, email, telephone)
+information()
